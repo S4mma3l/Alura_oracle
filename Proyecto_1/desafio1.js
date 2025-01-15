e452b2c8f93f39a4b681b18f20644a03a9c272a8
s4mma3l <lineartnw17@aol.com>
2025-01-15 16:58:59 -0600

    15-01-2025

---
+++ Proyecto_1/desafio1.js
@@ -0,0 +1,17 @@
+let mensajeBienvenida = "¡Bienvenida y bienvenido a nuestro sitio web!";
+alert(mensajeBienvenida)
+
+let nombre = "Lua";
+let Edad = "25";
+let numeroDeVentas = "50";
+let saldoDisponible = "1000";
+
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError)
+
+let nombreUsuario = prompt("Ingresa tu nombre, por favor: ");
+let edad = prompt("Ingresa tu edad, por favor: ");
+
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!");
+}

7c2b80191d2621f1acd48cdaaba0d4c6a1381864
s4mma3l <lineartnw17@aol.com>
2025-01-16 12:31:53 -0600

    resultos retos desafio 2

--- Practicas/desafio2/desafio2.js
+++ Practicas/desafio2/desafio2.js
@@ -1,9 +1,41 @@
-const numero = prompt("que dia de la semana es hoy?");
+//Desafio 2
+// reto uno
 
-if (numero > 0) {
-  console.log("El número es positivo");
-} else if (numero < 0) {
-  console.log("El número es negativo");
-} else {
-  console.log("El número es cero");
+let diaSemana = prompt("que dia de la semana es hoy?");
+
+if (diaSemana.toLocaleLowerCase() === "sabado" || diaSemana.toLowerCase() === "domingo" ) {
+  alert("¡Buen fin de semana!");
+} else  {
+  alert("¡Buena semana!");
 }
+
+// reto dos
+
+let userNumber = prompt("Ingrese un numero, por favor: ");
+
+if (userNumber >= 0) {
+  alert("Tu numero es positivo.");
+} else {
+  alert("Tu numero es negativo.");
+}
+
+// reto tres
+
+let puntosDelJuego = prompt("Ingresa la cantidad de puntos que obtuviste en tu juego, por favor:");
+
+if (puntosDelJuego >= 100) {
+  alert("Felicidades, has ganado!");
+} else {
+  alert("Intentalo nuevamente para ganar!")
+}
+
+// reto cuatro
+
+let saldoCuenta = ("72000")
+
+alert("El saldo en tu cuenta es de: " + saldoCuenta);
+
+// reto cinco
+const userName = prompt("Por favor, brindame tu nombre:");
+
+alert("Bienvenido/a " + userName + "!");
